8e42afe000c9ba2b1cd8019160c47b058c349af4
Felix <felixsavero78@gmail.com>
2024-06-28 17:08:41 +0700

    feat: query builder insert

---
+++ tests/Feature/QueryBuilderTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use Tests\TestCase;
+
+class QueryBuilderTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        DB::delete('DELETE FROM categories');
+    }
+
+    public function testInsert()
+    {
+        DB::table('categories')->insert([
+            'id' => 'GADGET',
+            'name' => 'Gadget'
+        ]);
+
+        DB::table('categories')->insert([
+            'id' => 'FOOD',
+            'name' => 'Food'
+        ]);
+
+        $result = DB::select('SELECT COUNT(id) AS total FROM categories');
+        $this->assertEquals(2, $result[0]->total);
+    }
+}
