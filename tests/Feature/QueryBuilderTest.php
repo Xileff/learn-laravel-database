c97366203d26a57cba0f02a3edb32c4ce4968ccd
Felix <felixsavero78@gmail.com>
2024-06-28 18:08:38 +0700

    feat: query builder where

--- tests/Feature/QueryBuilderTest.php
+++ tests/Feature/QueryBuilderTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Database\Query\Builder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\DB;
@@ -43,5 +44,144 @@ class QueryBuilderTest extends TestCase
         $result->each(function ($item) {
             Log::info(json_encode($item));
         });
+    }
+
+    // helper function for creating 4 dummy data
+    public function insertCategories()
+    {
+        DB::table('categories')->insert([
+            'id' => 'GADGET',
+            'name' => 'Gadget',
+            'created_at' => '2020-10-10 10:10:10'
+        ]);
+
+        DB::table('categories')->insert([
+            'id' => 'FOOD',
+            'name' => 'Food',
+            'created_at' => '2020-10-10 10:10:10'
+        ]);
+
+        DB::table('categories')->insert([
+            'id' => 'LAPTOP',
+            'name' => 'Laptop',
+            'created_at' => '2020-10-10 10:10:10'
+        ]);
+
+        DB::table('categories')->insert([
+            'id' => 'FASHION',
+            'name' => 'Fashion',
+            'created_at' => '2020-10-10 10:10:10'
+        ]);
+
+        DB::table('categories')->insert([
+            'id' => 'SMARTPHONE',
+            'name' => 'Smartphone',
+            'created_at' => '2020-10-10 10:10:10'
+        ]);
+    }
+
+    // helper function for asserting amount and logging
+    public function checkCountAndLog($expectedAmount, $result)
+    {
+        $this->assertCount($expectedAmount, $result);
+
+        $result->each(function ($item) {
+            Log::info(json_encode($item));
+        });
+    }
+
+    // SELECT * FROM categories WHERE name = ? OR name = ?
+    public function testWhereOrBiasa()
+    {
+        $this->insertCategories();
+
+        $result = DB::table('categories')
+            ->where('name', '=', 'Gadget')
+            ->orWhere('name', '=', 'Food')
+            ->get();
+
+        $this->checkCountAndLog(2, $result);
+    }
+
+    // SELECT * FROM categories WHERE (name = ? OR name = ?)
+    public function testWhereOrPakeKurung()
+    {
+        $this->insertCategories();
+
+        $result = DB::table('categories')->where(function (Builder $builder) {
+            $builder->where('name', '=', 'Gadget');
+            $builder->orWhere('name', '=', 'Food');
+        })->get();
+
+        $this->checkCountAndLog(2, $result);
+    }
+
+    // SELECT * FROM categories WHERE name = ? AND name = ?
+    public function testWhereAndBiasa()
+    {
+        $this->insertCategories();
+
+        $result = DB::table('categories')
+            ->where('id', '=', 'GADGET')
+            ->where('name', '=', 'Gadget')
+            ->get();
+
+        $this->checkCountAndLog(1, $result);
+    }
+
+    // SELECT * FROM categories WHERE (name = ? AND name = ?)
+    public function testWhereAndPakeKurung()
+    {
+        $this->insertCategories();
+
+        $result = DB::table('categories')->where(function (Builder $builder) {
+            $builder
+                ->where('id', '=', 'GADGET')
+                ->where('name', '=', 'Gadget');
+        })->get();
+
+        $this->checkCountAndLog(1, $result);
+    }
+
+    // SELECT * from categories WHERE created_at between ? and ?
+    public function testWhereBetween()
+    {
+        $this->insertCategories();
+
+        $result = DB::table('categories')->whereBetween('created_at', ['2020-09-01 00:00:00', '2020-11-01 00:00:00'])->get();
+
+        $this->checkCountAndLog(5, $result);
+    }
+
+    // SELECT * FROM categories WHERE id IN (?, ?) 
+    public function testWhereIn()
+    {
+        $this->insertCategories();
+
+        $result = DB::table('categories')->whereIn('id', ['SMARTPHONE', 'LAPTOP'])->get();
+
+        $this->checkCountAndLog(2, $result);
+    }
+
+    // SELECT * FROM categories WHERE description IS NULL
+    public function testWhereNull()
+    {
+        $this->insertCategories();
+
+        $result = DB::table('categories')->whereNull('description')->get();
+
+        $this->checkCountAndLog(5, $result);
+    }
+
+    // SELECT * FROM categories WHERE DATE(created_at) = ?
+    public function testWhereDate()
+    {
+        $this->insertCategories();
+
+        $result = DB::table('categories')
+            ->whereDate('created_at', '=', '2020-10-10')
+            ->get();
+
+        $this->checkCountAndLog(5, $result);
     }
 }
